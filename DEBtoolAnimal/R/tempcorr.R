5d765ae1b85ca86b207313b065a8344ce50015de
Laure Pecquerie <laure.pecquerie@ird.fr>
2015-12-09 08:50:05 +1100

    Units, typos and changes in the Details section
    I think we should write the full formula for the 3 param and 5 param,
    although it looks cumbersome. I know a lot of people get this equation
    wrong. They forget s(T_1)

--- DEBtoolAnimal/R/tempcorr.R
+++ DEBtoolAnimal/R/tempcorr.R
@@ -2,18 +2,19 @@
 #'
 #' @description Calculates the factor with which physiological rates should be multiplied to go from a reference temperature to a given temperature
 #' @family miscelaneous functions
-#' @param Temp vector with new temperatures
-#' @param T_1 scalar with reference temperature
-#' @param T_A scalar with Arrhenius temperature
-#' @param T_L (optional) scalar with lower boundary of temperature range
-#' @param T_AL (optional) scalar with Arrhenius temperature for lower boundary of temperature range
-#' @param T_H (optional) scalar with upper boundary of temperature range
-#' @param T_AH (optional) scalar with Arrhenius temperature for upper boundary of temperature range
+#' @param Temp vector with temperatures (in Kelvin degrees)
+#' @param T_1 scalar with reference temperature (in Kelvin degrees)
+#' @param T_A scalar with Arrhenius temperature (in Kelvin degrees)
+#' @param T_L (optional) scalar with lower boundary of temperature range (in Kelvin degrees)
+#' @param T_AL (optional) scalar with Arrhenius temperature for lower boundary of temperature range (in Kelvin degrees)
+#' @param T_H (optional) scalar with upper boundary of temperature range (in Kelvin degrees)
+#' @param T_AH (optional) scalar with Arrhenius temperature for upper boundary of temperature range (in Kelvin degrees)
 #' @return vector with temperature correction factors that affect all rates
-#' @details Temperature impacts metabolic rates. These impact is modeled in its most simplest way (1 parameter) performing the folowing correction to the time-dependent parameters \eqn{\dot{\theta}}{\theta}:
+#' @details Temperature impacts metabolic rates. This impact,in its most simplest way (1 parameter), is modeled performing the following correction to the time-dependent parameter \eqn{\dot{\theta}}{\theta}:
 #' \deqn{\dot{\theta}(T) = \dot{\theta}(T_1) \exp\left(\frac{T_A}{T_1} - \frac{T_A}{T}\right)}{\theta(T) = \theta(T_1) exp(T_A/ T_1 - T_A/ T)}
 #' For a more detailed modeling one can multiply with an extra fraction \eqn{s(T_1)/s(T)} with (3 parameters):
-#' \deqn{s(T) = 1 + \exp\left(\frac{T_{AL}}{T} - \frac{T_{AL}}{T_L}\right}{s(T) = 1 + exp(T_AL/ T - T_AL/ T_L))}
+#' \deqn{\dot{\theta}(T) = \dot{\theta}(T_1) \exp\left(\frac{T_A}{T_1} - \frac{T_A}{T}\right){s(T_1)/s(T)}}{\theta(T) = \theta(T_1) exp(T_A/ T_1 - T_A/ T){s(T_1)/s(T)}}
+#' with \deqn{s(T) = 1 + \exp\left(\frac{T_{AL}}{T} - \frac{T_{AL}}{T_L}\right}{s(T) = 1 + exp(T_AL/ T - T_AL/ T_L))}
 #' or (5 parameters)
 #' \deqn{s(T) = 1 + \exp\left(\frac{T_{AL}}{T} - \frac{T_{AL}}{T_L}\right + \exp\left(\frac{T_{AH}}{T_H} - \frac{T_{AH}}{T}\right}{s(T) = 1 + exp(T_AL/ T - T_AL/ T_L) + exp(T_AH/ T_H - T_AH/ T)}
 #' @examples tempcorr(c(330, 331, 332), 320, T_A = 12000, T_L = 277, T_H = 331, T_AL = 20000, T_AH = 190000)
