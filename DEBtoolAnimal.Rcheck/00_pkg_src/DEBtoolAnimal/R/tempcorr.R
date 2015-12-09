c0be8d4cc3995eefd6d35921d30b4b0c6ef70b0e
GMMarques <goncalo.marques@tecnico.ulisboa.pt>
2015-12-09 08:01:41 +0000

    changes in tempcorr header II

--- DEBtoolAnimal.Rcheck/00_pkg_src/DEBtoolAnimal/R/tempcorr.R
+++ DEBtoolAnimal.Rcheck/00_pkg_src/DEBtoolAnimal/R/tempcorr.R
@@ -2,16 +2,16 @@
 #'
 #' @description Calculates the factor with which physiological rates should be multiplied to go from a reference temperature to a given temperature
 #' @family miscelaneous functions
-#' @param Temp vector with new temperatures
-#' @param T_1 scalar with reference temperature
-#' @param T_A scalar with Arrhenius temperature
-#' @param T_L (optional) scalar with lower boundary of temperature range
-#' @param T_AL (optional) scalar with Arrhenius temperature for lower boundary of temperature range
-#' @param T_H (optional) scalar with upper boundary of temperature range
-#' @param T_AH (optional) scalar with Arrhenius temperature for upper boundary of temperature range
+#' @param Temp vector with temperatures (in Kelvin)
+#' @param T_1 scalar with reference temperature (in Kelvin)
+#' @param T_A scalar with Arrhenius temperature (in Kelvin)
+#' @param T_L optional scalar with lower boundary of temperature range (in Kelvin)
+#' @param T_AL optional scalar with Arrhenius temperature for lower boundary of temperature range (in Kelvin)
+#' @param T_H optional scalar with upper boundary of temperature range (in Kelvin)
+#' @param T_AH optional scalar with Arrhenius temperature for upper boundary of temperature range (in Kelvin)
 #' @return vector with temperature correction factors that affect all rates
-#' @details Temperature impacts metabolic rates. These impact is modeled in its most simplest way (1 parameter) performing the folowing correction to the time-dependent parameters \eqn{\dot{\theta}}{\theta}:
-#' \deqn{\dot{\theta}(T) = \dot{\theta}(T_1) \exp\left(\frac{T_A}{T_1} - \frac{T_A}{T}\right)}{\theta(T) = \theta(T_1) exp(T_A/ T_1 - T_A/ T)}
+#' @details Temperature impacts metabolic rates. This impact, in its most simplest way (1 parameter), is modeled by multiplying all the time-dependent parameters by a correction factor:
+#' \deqn{\exp\left(\frac{T_A}{T_1} - \frac{T_A}{T}\right)}{exp(T_A/ T_1 - T_A/ T)}
 #' For a more detailed modeling one can multiply with an extra fraction \eqn{s(T_1)/s(T)} with (3 parameters):
 #' \deqn{s(T) = 1 + \exp\left(\frac{T_{AL}}{T} - \frac{T_{AL}}{T_L}\right}{s(T) = 1 + exp(T_AL/ T - T_AL/ T_L))}
 #' or (5 parameters)
