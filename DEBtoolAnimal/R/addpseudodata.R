f3ba7c6122b59519888437fd3ac34c48e4696a89
GMMarques <goncalo.marques@tecnico.ulisboa.pt>
2015-12-30 17:08:24 +0000

    Converted mydata_my_pet
    Converted mydata_my_pet.
    Also converted functions used in mydata_my_pet: setweights and
    addpseudodata

---
+++ DEBtoolAnimal/R/addpseudodata.R
@@ -0,0 +1,36 @@
+#' Adds pseudodata information into inputed data structures
+#'
+#' @description Adds the pseudodata information and weights for purposes of the regression
+#' @family add-my-pet auxiliary functions
+#' @param data structure with data
+#' @param units structure with data units
+#' @param label structure with data labels
+#' @param weights structure with weights
+#' @return structures with data, units, labael and weights
+#' @examples [data, units, label, weight] <- addpseudodata();
+#' @export
+addpseudodata <- function(data = list(), units = list(), label = list(), weights = list()){
+  # set pseudodata
+  data$psd$v <- 0.02;     units$psd$v <- "cm/d";       label$psd$v <- "energy conductance";
+  data$psd$kap <- 0.8;    units$psd$kap <- "-";        label$psd$kap <- "allocation fraction to soma";
+  data$psd$kap.R <- 0.95; units$psd$kap.R <- "-";      label$psd$kap.R <- "reproduction efficiency";
+  data$psd$p.M <- 18;     units$psd$p.M <- "J/d.cm^3"; label$psd$p.M <- "vol-spec som maint";
+  data$psd$p.T <- 0;      units$psd$p.T <- "J/d.cm^2"; label$psd$p.T <- "surf-spec som maint";
+  data$psd$k.J <- 0.002;  units$psd$k.J <- "1/d";      label$psd$k.J <- "maturity maint rate coefficient";
+  data$psd$kap.G <- 0.8;  units$psd$kap.G <- "-";      label$psd$kap.G <- "growth efficiency";
+
+  # set weights
+  weights$psd <- setweights(data$psd);
+  weights$psd$v     <- 0.1 * weights$psd$v;
+  weights$psd$kap   <- 0.1 * weights$psd$kap;
+  weights$psd$kap.R <- 0.1 * weights$psd$kap.R;
+  weights$psd$p.M   <- 0.1 * weights$psd$p.M;
+  weights$psd$p.T   <- 0.1 * weights$psd$p.T;
+  weights$psd$k.J   <- 0.1 * weights$psd$k.J;
+  weights$psd$kap.G <- 0.1 * weights$psd$kap.G;
+
+  weights$psd$kap.G <- 200 * weights$psd$kap.G;   # more weight to kap.G
+
+  return(list(data, units, label, weights))
+
+}
---
+++ DEBtoolAnimal/R/setweights.R
@@ -0,0 +1,33 @@
+#' Sets automatically the weights for the data (to be used in a regression)
+#'
+#' @description computes weights for given data and adds it to the weight structure
+#' @family add-my-pet auxiliary functions
+#' @param data structure with data
+#' @param weights structure with weights
+#' @return structure with weights
+#' @details computes weights for given data and adds it to the weight structure
+#' for the zero-variate data y the weight will be
+#' \deqn{min(100, 1/ max(10^-6, y) ^2 \right)}{\min\left(100, \frac{1}{\max\left(10^{-6}, y\right ) ^2}}
+#' for the uni-variate data y the weight will be
+#' \deqn{1/ N \bar{y}^2}{\frac{1}{N \bar{y}^2}}
+#' @examples setweights(data)
+#' @export
+setweights <- function(data, weights = list()){
+  nm <- names(data); # vector of cells with names of data sets
+
+  for (dti in nm){
+    dtSet = get(dti, data);
+    if (is.null(weights[[dti]])) {
+      if (length(dtSet) == 1) {
+        weights[[dti]] <- min(100, 1/ max(1e-6, dtSet)^ 2);
+      } else {
+        ndt = dim(dtSet);
+        meanval = mean(dtSet[,1]);
+        weights[[dti]] <- rep(1/ meanval^2/ ndt[1], ndt[1]);
+      }
+    }
+  }
+
+  return(weights)
+
+}
