d4a83184e6d22c3f9f1f5c8c6c6f7d4897d9a5ea
GMMarques <goncalo.marques@tecnico.ulisboa.pt>
2016-01-04 22:13:45 +0000

    Converted estim_options and nmregr_options

---
+++ DEBtoolAnimal/R/estim_options.R
@@ -0,0 +1,162 @@
+#' Sets options for estim_pars
+#'
+#' @description Sets options for estimation one by one
+#' @family regression functions
+#' @param key string with option to set
+#' @param val value of the option
+#' @details no input: print values to screen
+#'
+#' one input:
+#'
+#' * "default": sets options at default values
+#' * any other key (see below): print value to screen
+#'
+#' two inputs:
+#'
+#' * "filter": 1 - use filter (default); 0 - do not;
+#' * "pars_init_method":
+#'    0 - get initial estimates from automatized computation (default)
+#'    1 - read initial estimates from .mat file (for continuation)
+#'    2 - read initial estimates from pars_init file
+#' * "pseudodata_pets":
+#'    0 - put pseudodata together with data (default)
+#'    1 - put it apart (only for multispecies estimation)
+#' * "results_output":
+#'    0 - prints results to screen (default)
+#'    1 - prints results to screen, saves to .mat file
+#'    2 - saves data to .mat file and graphs to .png files
+#'    (prints results to screen using a customized results file when it exists)
+#'  * "method": "nm" - use Nelder-Mead method; "no" - do not estimate;
+#'
+#'  for other options see corresponding options file of the method (e.g. nmregr_options)
+#' @examples estim_options("default")
+#' @export
+estim_options <- function(key = "inexistent", val = ""){
+
+  switch(key,
+         default = {
+           assign("filter", 1, envir = .GlobalEnv)
+           assign("pars_init_method", 0, envir = .GlobalEnv)
+           assign("pseudodata_pets", 0, envir = .GlobalEnv)
+           assign("results_output", 0, envir = .GlobalEnv)
+           assign("method", "nm", envir = .GlobalEnv)
+           nmregr_options("default");
+         },
+         filter = {
+           if(is.numeric(val)){
+             assign("filter", val, envir = .GlobalEnv)
+           } else {
+             if(is.numeric(filter))
+               cat("filter = ", filter, "\n", sep = "")
+             else
+               cat("filter = unknown \n")
+             cat("0 - do not use filter \n");
+             cat("1 - use filter \n");
+           }
+         },
+         pars_init_method = {
+           if(is.numeric(val)){
+             assign("pars_init_method", val, envir = .GlobalEnv)
+           } else {
+             if(exists("pars_init_method"))
+               cat("pars_init_method = ", pars_init_method, "\n", sep = "")
+             else
+               cat("pars_init_method = unknown \n")
+             cat("0 - get initial estimates from automatized computation \n");
+             cat("1 - read initial estimates from .mat file \n");
+             cat("2 - read initial estimates from pars_init file \n");
+             cat("3 - prints results using a customized results file \n");
+           }
+         },
+         pseudodata_pets = {
+           if(is.numeric(val)){
+             assign("pseudodata_pets", val, envir = .GlobalEnv)
+           } else {
+             if(exists("pseudodata_pets"))
+               cat("pseudodata_pets = ", pseudodata_pets, "\n", sep = "")
+             else
+               cat("pseudodata_pets = unknown \n")
+             cat("0 - put pseudodata together with data \n");
+             cat("1 - put it apart (for multispecies estimation) \n");
+           }
+         },
+         results_output = {
+           if(is.numeric(val)){
+             assign("results_output", val, envir = .GlobalEnv)
+           } else {
+             if(exists("results_output"))
+               cat("results_output = ", results_output, "\n", sep = "")
+             else
+               cat("results_output = unknown \n")
+             cat("0 - prints results to screen \n");
+             cat("1 - prints results, saves to matlab file and produces html \n");
+             cat("2 - read initial estimates from pars_init file \n");
+             cat("2 - saves to matlab file and produces html \n");
+           }
+         },
+         method = {
+           if(nchar(val) != 0){
+             assign("method", val, envir = .GlobalEnv)
+           } else {
+             if(exists("method"))
+               cat("method = ", method, "\n", sep = "")
+             else
+               cat("method = unknown \n")
+             cat("'no' - do not estimate \n");
+             cat("'nm' - use Nelder-Mead method \n");
+           }
+         },
+         inexistent = {
+           if(is.numeric(filter))
+             cat("filter = ", filter, "\n", sep = "")
+           else
+             cat("filter = unknown \n")
+           if(exists("pars_init_method"))
+             cat("pars_init_method = ", pars_init_method, "\n", sep = "")
+           else
+             cat("pars_init_method = unknown \n")
+           if(exists("pseudodata_pets"))
+             cat("pseudodata_pets = ", pseudodata_pets, "\n", sep = "")
+           else
+             cat("pseudodata_pets = unknown \n")
+           if(exists("results_output"))
+             cat("results_output = ", results_output, "\n", sep = "")
+           else
+             cat("results_output = unknown \n")
+           if(exists("method")) {
+             cat("method = ", method, sep = "")
+             if(method != "no")
+               eval(parse(text = paste(method, "regr_options", sep = "")))
+           }
+           else
+             cat("method = unknown")
+         },
+         {
+         cat(paste("key ", key, " is unkown", sep = ""))
+         if(is.numeric(filter))
+           cat("filter = ", filter, "\n", sep = "")
+         else
+           cat("filter = unknown \n")
+         if(exists("pars_init_method"))
+           cat("pars_init_method = ", pars_init_method, "\n", sep = "")
+         else
+           cat("pars_init_method = unknown \n")
+         if(exists("pseudodata_pets"))
+           cat("pseudodata_pets = ", pseudodata_pets, "\n", sep = "")
+         else
+           cat("pseudodata_pets = unknown \n")
+         if(exists("results_output"))
+           cat("results_output = ", results_output, "\n", sep = "")
+         else
+           cat("results_output = unknown \n")
+         if(exists("method")) {
+           cat("method = ", method, sep = "")
+           if(method != "no")
+             eval(parse(text = paste(method, "regr_options", sep = "")))
+         }
+         else
+           cat("method = unknown")
+         }
+  )
+
+  }
---
+++ DEBtoolAnimal/R/nmregr_options.R
@@ -0,0 +1,152 @@
+#' Sets options for function nmregr
+#'
+#' @description Sets options for estimation one by one
+#' @family regression functions
+#' @param key string with option to set
+#' @param val value of the option
+#' @details no input: print values to screen
+#'
+#' one input:
+#'
+#' * "default": sets options at default values
+#' * any other key (see below): print value to screen
+#'
+#' two inputs:
+#'
+#' * "report": 1 - to report steps to screen; 0 - not to;
+#' * "max_step_number": maximum number of steps
+#' * "max_fun_evals": maximum number of function evaluations
+#' * "tol_simplex": tolerance for how close the simplex points must be together to call them the same
+#' * "tol_tun": tolerance for how close the loss-function values must be together to call them the same
+#' * "simplex_size": fraction added (subtracted if negative) to the free parameters when building the simplex
+#' @examples nmregr_options("default")
+#' @export
+nmregr_options <- function(key = "inexistent", val = ""){
+
+  switch(key,
+         default = {
+           assign("report", 1, envir = .GlobalEnv)
+           assign("max_step_number", 500, envir = .GlobalEnv)
+           assign("max_fun_evals", 2000, envir = .GlobalEnv)
+           assign("tol_simplex", 1e-4, envir = .GlobalEnv)
+           assign("tol_fun", 1e-4, envir = .GlobalEnv)
+           assign("simplex_size", 0.05, envir = .GlobalEnv)
+         },
+         report = {
+           if(is.numeric(val)){
+             assign("report", val, envir = .GlobalEnv)
+           } else {
+             if(exists("report"))
+               cat("report = ", report, "\n", sep = "")
+             else
+               cat("report = unknown \n")
+             cat("0 - do not report the regression steps \n");
+             cat("1 - report the regression steps \n");
+           }
+         },
+         max_step_number = {
+           if(is.numeric(val)){
+             assign("max_step_number", val, envir = .GlobalEnv)
+           } else {
+             if(exists("max_step_number"))
+               cat("max_step_number = ", max_step_number, "\n", sep = "")
+             else
+               cat("max_step_number = unknown \n")
+           }
+         },
+         max_fun_evals = {
+           if(is.numeric(val)){
+             assign("max_fun_evals", val, envir = .GlobalEnv)
+           } else {
+             if(exists("max_fun_evals"))
+               cat("max_fun_evals = ", max_fun_evals, "\n", sep = "")
+             else
+               cat("max_fun_evals = unknown \n")
+           }
+         },
+         tol_simplex = {
+           if(is.numeric(val)){
+             assign("tol_simplex", val, envir = .GlobalEnv)
+           } else {
+             if(exists("tol_simplex"))
+               cat("tol_simplex = ", tol_simplex, "\n", sep = "")
+             else
+               cat("tol_simplex = unknown \n")
+           }
+         },
+         tol_fun = {
+           if(is.numeric(val)){
+             assign("tol_fun", val, envir = .GlobalEnv)
+           } else {
+             if(exists("tol_fun"))
+               cat("tol_fun = ", tol_fun, "\n", sep = "")
+             else
+               cat("tol_fun = unknown \n")
+           }
+         },
+         simplex_size = {
+           if(is.numeric(val)){
+             assign("simplex_size", val, envir = .GlobalEnv)
+           } else {
+             if(exists("simplex_size"))
+               cat("simplex_size = ", simplex_size, "\n", sep = "")
+             else
+               cat("simplex_size = unknown \n")
+           }
+         },
+         inexistent = {
+           if(exists("report"))
+             cat("report = ", pars_init_method, "\n", sep = "")
+           else
+             cat("report = unknown \n")
+           if(exists("max_step_number"))
+             cat("max_step_number = ", pars_init_method, "\n", sep = "")
+           else
+             cat("max_step_number = unknown \n")
+           if(exists("max_fun_evals"))
+             cat("max_fun_evals = ", pars_init_method, "\n", sep = "")
+           else
+             cat("max_fun_evals = unknown \n")
+           if(exists("tol_simplex"))
+             cat("tol_simplex = ", pseudodata_pets, "\n", sep = "")
+           else
+             cat("tol_simplex = unknown \n")
+           if(exists("tol_fun"))
+             cat("tol_fun = ", results_output, "\n", sep = "")
+           else
+             cat("simplex_size = unknown \n")
+           if(exists("simplex_size"))
+             cat("simplex_size = ", results_output, "\n", sep = "")
+           else
+             cat("simplex_size = unknown \n")
+         },
+         {
+         cat(paste("key ", key, " is unkown", sep = ""))
+         if(exists("report"))
+           cat("report = ", pars_init_method, "\n", sep = "")
+         else
+           cat("report = unknown \n")
+         if(exists("max_step_number"))
+           cat("max_step_number = ", pars_init_method, "\n", sep = "")
+         else
+           cat("max_step_number = unknown \n")
+         if(exists("max_fun_evals"))
+           cat("max_fun_evals = ", pars_init_method, "\n", sep = "")
+         else
+           cat("max_fun_evals = unknown \n")
+         if(exists("tol_simplex"))
+           cat("tol_simplex = ", pseudodata_pets, "\n", sep = "")
+         else
+           cat("tol_simplex = unknown \n")
+         if(exists("tol_fun"))
+           cat("tol_fun = ", results_output, "\n", sep = "")
+         else
+           cat("simplex_size = unknown \n")
+         if(exists("simplex_size"))
+           cat("simplex_size = ", results_output, "\n", sep = "")
+         else
+           cat("simplex_size = unknown \n")
+         }
+  )
+
+  }
