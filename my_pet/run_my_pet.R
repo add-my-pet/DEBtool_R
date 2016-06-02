6bc9665f68a9ec0aa02f50b8337198dfd88bbab2
GMMarques <goncalo.marques@tecnico.ulisboa.pt>
2016-06-02 17:33:34 +0100

    Moving run_my_pet

--- my_pet/run_my_pet.R
+++ my_pet/run_my_pet.R
@@ -1,8 +1,20 @@
 
 rm(list = ls());
 
+require(DEBtoolAnimal)
+require(deSolve)
+
 pets <<- c("my_pet");
 # check_my_pet(pets);  # check pet-files for required fields
+
+petsNumber <- length(pets)
+if(petsNumber == 1) {
+  # set your own working directory. This directory should contain the species folder with the add-my-pet files
+  # setwd(paste('../',pets, sep = ""))
+  source(paste('pars_init_', pets, '.R',sep = ""))
+  source(paste('mydata_', pets, '.R', sep = ""))
+  source(paste('predict_', pets, '.R', sep = ""))
+}
 
 
 # See estim_options for more options
@@ -18,13 +30,13 @@ estim_options("default"); # runs estimation, uses nmregr method and filter
 #                     (prints results to screen using a customized results file when there is one)
 # "report"        :   0 - does not print to screen the step numbers and the corresponding simplex ssq values
 
-estim_options("max_step_number",5e3); # set options for parameter estimation
+estim_options("max_step_number",2e2); # set options for parameter estimation
 estim_options("max_fun_evals",5e3);   # set options for parameter estimation
 #estim_options("report",0);           # save time during the estimation
 
 estim_options("pars_init_method", 2);
 estim_options("results_output", 0);
-estim_options("method", "no");
+estim_options("method", "nm");
 
 estim_pars(); # run estimation
 
